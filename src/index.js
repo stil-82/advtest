39dd59d7280dddb736f954673d40e304cab91554
Ivan Shcherbakov <stil_82@mail.ru>
2018-06-16 13:34:08 +0700

    dist and src

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from './modules/sum';
+import guard from './modules/guard';
+
+console.log(sum(15,178));
+console.log(guard('Ivanes'));
---
+++ src/modules/guard.js
@@ -0,0 +1,7 @@
+export default function (name) {
+    if (name == 'Ivan') {
+        return 'hello';
+} else {
+    return 'get out!';
+}}
+;
